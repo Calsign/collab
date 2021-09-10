5fd886280407f4f24a3419b2f334681630b7074a
Will Smith <wds68@cornell.edu>
2021-09-09 22:10:38 -0700

    add null byte test

--- tests/main.rs
+++ tests/main.rs
@@ -1,6 +1,3 @@
-use std::thread::sleep;
-use std::time::Duration;
-
 use test_common::{common::Result, dir, file, files, rig};
 
 #[test]
@@ -87,6 +84,28 @@ fn send_back() -> Result<()> {
 
     assert_eq!(files2, files::load_dir(&root1)?);
     assert_eq!(files2, files::load_dir(&root2)?);
+
+    return Ok(());
+}
+
+#[test]
+fn null_byte() -> Result<()> {
+    let root1 = rig::tempdir()?;
+    let root2 = rig::tempdir()?;
+
+    let daemon1 = rig::daemon("r1", &root1)?;
+    let _daemon2 = rig::connect("r2", &root2, &daemon1)?;
+
+    let files = dir! {
+        "foobar" => file!(r"data\0\0more data")
+    };
+
+    files.apply(&root1)?;
+
+    rig::wait();
+
+    assert_eq!(files, files::load_dir(&root1)?);
+    assert_eq!(files, files::load_dir(&root2)?);
 
     return Ok(());
 }
