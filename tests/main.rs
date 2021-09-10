36c3d1d6f9ce4cb8f66ecd36291339484634c140
Will Smith <wds68@cornell.edu>
2021-09-09 22:22:25 -0700

    add chmod test

--- tests/main.rs
+++ tests/main.rs
@@ -1,4 +1,6 @@
-use test_common::{common::Result, dir, file, files, rig};
+use std::fs;
+
+use test_common::{common::Result, dir, file, files, path, rig};
 
 #[test]
 fn existing_files() -> Result<()> {
@@ -106,6 +108,46 @@ fn null_byte() -> Result<()> {
 
     assert_eq!(files, files::load_dir(&root1)?);
     assert_eq!(files, files::load_dir(&root2)?);
+
+    return Ok(());
+}
+
+#[test]
+#[cfg(target_family = "unix")]
+fn chmod() -> Result<()> {
+    use std::os::unix::fs::PermissionsExt;
+
+    let root1 = rig::tempdir()?;
+    let root2 = rig::tempdir()?;
+
+    let daemon1 = rig::daemon("r1", &root1)?;
+    let _daemon2 = rig::connect("r2", &root2, &daemon1)?;
+
+    let files = dir! {
+        "foobar" => file!("foobar")
+    };
+
+    files.apply(&root1)?;
+
+    rig::wait();
+
+    assert_eq!(files, files::load_dir(&root1)?);
+    assert_eq!(files, files::load_dir(&root2)?);
+
+    let path = path!(&root1, "foobar");
+    let mut perms1 = fs::metadata(&path)?.permissions();
+    perms1.set_mode(0o755);
+    fs::set_permissions(&path, perms1)?;
+
+    rig::wait();
+
+    assert_eq!(files, files::load_dir(&root1)?);
+    assert_eq!(files, files::load_dir(&root2)?);
+
+    let perms2 = fs::metadata(&path)?.permissions();
+
+    // the code currently only sets the user execute bit
+    assert_eq!(0o700, perms2.mode() & 0o700);
 
     return Ok(());
 }
